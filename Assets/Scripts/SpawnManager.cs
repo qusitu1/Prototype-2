7df98d524d6e5cca94fc46ac40d909365787c311
qusitu1 <122053882+qusitu1@users.noreply.github.com>
2023-01-20 12:26:58 -0800

    animal spawn manager fix (prefabs)

--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class SpawnManager : MonoBehaviour
 {
     public GameObject[] animalPrefabs;
+    
     // Start is called before the first frame update
     void Start()
     {
@@ -14,6 +15,11 @@ public class SpawnManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            int animalIndex = Random.Range(0, animalPrefabs.Length);
+            
+            Instantiate(animalPrefabs[animalIndex], new Vector3(0, 0, 20), animalPrefabs[animalIndex].transform.rotation);
+        }
     }
 }
